a0c181d9e0e8996f7c79532b881441dd521706df
Cryptexfx-web <4kccfdvzzm@privaterelay.appleid.com>
2025-12-15 21:48:45 +0100

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = express.Router();
+
+router.post("/register", async (req,res)=>{
+  const hash = await bcrypt.hash(req.body.password,10);
+  await new User({...req.body,password:hash}).save();
+  res.json({message:"Registered"});
+});
+
+router.post("/login", async (req,res)=>{
+  const user = await User.findOne({email:req.body.email});
+  if(!user) return res.sendStatus(400);
+  const ok = await bcrypt.compare(req.body.password,user.password);
+  if(!ok) return res.sendStatus(400);
+
+  const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET);
+  res.json({token,role:user.role});
+});
+
+module.exports = router;
